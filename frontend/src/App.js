a4e98afb7d850568e236483665f3d2532a17b9d4
Elitsa Mincheva <e.mincheva@gmail.com>
2024-03-30 18:27:36 -0400

    some more basic-ish frontend stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,71 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import "./App.css";
-import { useState } from 'react';
-import * as api from './api'
+import { Grid, TextField, Button } from '@mui/material';
+import * as api from './api';
+import RecipeCard from './components/RecipeCard';
+import TopMenuBar from './components/TopMenuBar';
 
-
-// our app
 function App() {
-    const [ingredients, setIngredients] = useState("");
-    const[recipes, setRecipes] = useState([]);
+    const [ingredients, setIngredients] = React.useState({
+        ingredient1: "",
+        ingredient2: "",
+        ingredient3: ""
+    });
+    const [recipes, setRecipes] = React.useState([]);
 
-    const handleSubmit = async () =>{
+    const handleSubmit = async () => {
         try { 
-            const recipes = await api.searchByIngredients(ingredients);
+            const ingredientsList = Object.values(ingredients).filter(ingredient => ingredient.trim() !== "").join(", ");
+            const recipes = await api.searchByIngredients(ingredientsList);
             setRecipes(recipes);
         } catch (error) {
             console.log(error);
         }
     }
 
-    const handleChange = (e) =>{
-        setIngredients(e.target.value);
+    const handleChange = (e) => {
+        setIngredients({
+            ...ingredients,
+            [e.target.name]: e.target.value
+        });
     }
 
     return (
-      <div>
-        <input type='text' onChange={handleChange}></input>
-       
-        <button onClick={handleSubmit}>Submit</button>
-        
-        
-        {recipes.map((recipe)=> (
-            <div>
-                <img src={recipe.image}></img>
-                {/* recipe image location: {recipe.image} */}
-                recipe title: {recipe.title}
-            </div>
-        ))}
-        
-      </div>
+        <Grid container spacing={2}>
+            <TopMenuBar/>
+            {/* Left column for text fields and submit button */}
+            <Grid item xs={4}>
+                {/* Nested grid container with direction set to column */}
+                <Grid container direction="column" spacing={2}>
+                    <Grid item>
+                        <TextField name="ingredient1" id="outlined-basic" label="Ingredient 1" variant="outlined" onChange={handleChange}/>
+                    </Grid>
+                    <Grid item>
+                        <TextField name="ingredient2" id="outlined-basic" label="Ingredient 2" variant="outlined" onChange={handleChange}/>
+                    </Grid>
+                    <Grid item>
+                        <TextField name="ingredient3" id="outlined-basic" label="Ingredient 3" variant="outlined" onChange={handleChange}/>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            {/* Right column for displaying recipes */}
+            <Grid item xs={8}>
+                <Grid container spacing={2}>
+                    {recipes.map((recipe, index) => (
+                        <Grid item xs={4} key={index}> 
+                            <RecipeCard
+                                title={recipe.title}
+                                image={recipe.image}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
     );
-  }
-  
-  export default App;
+}
+
+export default App;
---
+++ frontend/src/components/RecipeCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function RecipeCard({ title, image }) {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={title} // Using title as alt text
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
