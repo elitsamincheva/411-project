7101d1e05b057be6b9f8253b517cc7168cb0d53d
Danielle Hsieh <dhsieh@bu.edu>
2024-04-24 00:31:00 -0400

    frontend styling

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,91 +1,99 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
-import spotify_logo from '../images/spotify_logo.png';
+import spotify_logo from '../images/Spotify_Icon.png';
 
 function LoginPage() {
     const [typedText, setTypedText] = useState('');
-    const [textToType, setTextToType] = useState('Recipe and Playlist Generator');
+    const [textToType] = useState('Recipe and Playlist Generator');
     const [charIndex, setCharIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
     const typingSpeed = 100; // Delay between each character (in milliseconds)
+    const pauseDuration = 2000;
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            if (charIndex < textToType.length) {
-                setTypedText(prevText => prevText + textToType.charAt(charIndex));
-                setCharIndex(prevIndex => prevIndex + 1);
-            } else {
-                clearInterval(timer);
-            }
-        }, typingSpeed);
-
+        let timer;
+        if (!isPaused) {
+            timer = setInterval(() => {
+                if (charIndex <= textToType.length) {
+                    if (charIndex === textToType.length) {
+                        setIsPaused(true);
+                        setTimeout(() => {
+                            setTypedText('');
+                            setCharIndex(0);
+                            setIsPaused(false);
+                        }, pauseDuration);
+                    } else {
+                        setTypedText(prevText => prevText + textToType.charAt(charIndex));
+                        setCharIndex(prevIndex => prevIndex + 1);
+                    }
+                }
+            }, typingSpeed);
+        }
         return () => clearInterval(timer);
-    }, [charIndex, textToType]);
+    }, [charIndex, textToType, isPaused]);
 
-    const backgroundStyle = {
-        background: 'radial-gradient(circle, rgba(241,156,121,1) 0%, rgba(212,224,155,1) 100%)',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
-
-    const buttonContainerStyle = {
-        display: 'flex',
-        gap: '20px',
-        marginTop: '50px',
-    };
-
-    const headingContainerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '10px',
-        marginBottom: '20px',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
-
-    const titleStyle = {
-        fontSize: '64px',
-        color: '#F6F4D2',
-        fontFamily: 'Poppins, sans-serif',
-        margin: '10px',
-    };
-
-    const subTitleStyle = {
-        fontSize: '21px',
-        color: '#F6F4D2',
-        fontFamily: '"Roboto Mono", monospace',
-        margin: '0px',
-    };
-
-    const loginButtonStyle = {
-        padding: '10px 20px',
-        fontSize: '16px',
-        borderRadius: '30px',
-        backgroundColor: '#000',
-        color: '#F6F4D2',
-        textDecoration: 'none',
-        border: 'none',
-        cursor: 'pointer',
-        fontFamily: '"Roboto Mono", monospace',
-        transition: 'background-color 0.3s ease',
-        '&:hover': {
-            backgroundColor: '#1ED760',
+    const styles = {
+        background: {
+            background: 'radial-gradient(circle, rgba(241,156,121,1) 0%, rgba(212,224,155,1) 100%)',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        buttonContainer: {
+            display: 'flex',
+            gap: '20px',
+            marginTop: '30px',
+        },
+        headingContainer: {
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            marginBottom: '20px',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        title: {
+            fontSize: '64px',
+            color: '#F6F4D2',
+            fontFamily: 'Poppins, sans-serif',
+            margin: '10px',
+        },
+        subTitle: {
+            fontSize: '21px',
+            color: '#F6F4D2',
+            fontFamily: '"Roboto Mono", monospace',
+            margin: '0px',
+            height: '21px',
+        },
+        loginButton: {
+            padding: '10px 50px',
+            fontSize: '16px',
+            fontWeight: 'bold',
+            borderRadius: '30px',
+            backgroundColor: 'transparent',
+            color: '#F6F4D2',
+            textDecoration: 'none',
+            border: '2px solid #F6F4D2',
+            boxShadow: 'none',
+            cursor: 'pointer',
+            fontFamily: 'Poppins, sans-serif',
+            transition: 'background-color 0.3s ease',
         },
     };
 
     return (
-        <div style={backgroundStyle}>
-            <div style={headingContainerStyle}>
-                <h1 style={titleStyle}>BeatBite</h1>
-                <h2 style={subTitleStyle}>{typedText}</h2>
+        <div style={styles.background}>
+            <div style={styles.headingContainer}>
+                <h1 style={styles.title}>BeatBite</h1>
+                <h2 style={styles.subTitle}>{typedText}</h2>
             </div>
-            <div style={buttonContainerStyle}>
-                <Button component={Link} to='http://localhost:3000/login' variant="contained" style={loginButtonStyle}>
-                    <span> Login with Spotify </span>
-                    <img src={spotify_logo} alt="Spotify Logo" style={{ width: '30px', marginLeft: '10px' }} />
+            <div style={styles.buttonContainer}>
+                <Button component={Link} to='http://localhost:3000/login' variant="contained" style={styles.loginButton}>
+                    <img src={spotify_logo} alt="Spotify Logo" style={{ width: '21px', marginRight: '10px' }} />
+                    <span> Sign in with Spotify </span>
                 </Button>
             </div>
         </div>
